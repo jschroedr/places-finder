5dfa347a3fe187b616fbcf649eb93cf933a83a49
Jake Schroeder <jake_schroeder@outlook.com>
2021-10-17 17:20:48 -0400

    main menu

--- src/wpgp/MainMenu.php
+++ src/wpgp/MainMenu.php
@@ -9,17 +9,38 @@
  * @author   Jake Schroeder <jake_schroeder@outlook.com>
  * @license  GNU v3
  * @link     https://github.com/jschroedr/wp-google-places
+ * 
+ * see: https://developer.wordpress.org/plugins/settings/custom-settings-page/
  */
 
 namespace wpgp 
 {
+    /**
+     * Manages main options menu display and data collection
+     * 
+     * @category Admin
+     * @package  Wpgp
+     * @author   Jake Schroeder <jake_schroeder@outlook.com>
+     * @license  GNU v3
+     * @link     https://github.com/jschroedr/wp-google-places
+     */
     class MainMenu
     {
+
+        /**
+         * Initialize the main menu as part of WP admin initialization
+         * 
+         * @return void
+         */
         public static function init() : void 
         {
             add_action(
                 'admin_init',
                 [self::class, 'initSettings']
+            );
+            add_action(
+                'admin_menu',
+                [self::class, 'addMenuPage']
             );
         }
 
@@ -29,6 +50,11 @@ namespace wpgp
         const POST_TYPE_FIELD_NAME = 'wpgp_single_location_post_type';
         const OPTIONS_GROUP_NAME = 'wpgp_options';
 
+        /**
+         * Initialize the main options menu
+         * 
+         * @return void
+         */
         public static function initSettings() : void 
         {
             // Register a new setting for "wporg" page.
@@ -86,6 +112,18 @@ namespace wpgp
         }
 
         /**
+         * Gets the wp options name from the option label
+         * 
+         * @param $labelFor string
+         * 
+         * @return string
+         */
+        private static function _getFieldName(string $labelFor) : string
+        {
+            return self::OPTIONS_GROUP_NAME . '[' . $labelFor . ']';
+        }
+
+        /**
          * Displays the API Key Field in the Main Options Menu
          * 
          * @param $args array the field arguments
@@ -100,7 +138,7 @@ namespace wpgp
             ?>
             <input
                 id="<?php echo $labelFor;?>"
-                name="<?php echo self::OPTIONS_GROUP_NAME;?>[<?php echo $labelFor; ?>]"
+                name="<?php echo self::_getFieldName($labelFor);?>"
                 type="password"
                 value="<?php echo $value;?>"
             >
@@ -108,7 +146,11 @@ namespace wpgp
         }
 
         /**
+         * Display the post type selector.
          * 
+         * @param $args array the arguments for the field
+         * 
+         * @return void
          */
         function postTypeCallback(array $args) : void
         {
@@ -121,9 +163,9 @@ namespace wpgp
             <label for="<?php echo $labelFor;?>">Select Post Type</label>
             <select
                     id="<?php echo $labelFor; ?>"
-                    name="<?php echo self::OPTIONS_GROUP_NAME;?>[<?php echo $labelFor;?>]">
+                    name="<?php echo self::_getFieldName($labelFor);?>">
                 <?php 
-                foreach($postTypes as $postType) {
+                foreach ($postTypes as $postType) {
                     $selected = selected($value, $postType, false);
                     ?>
                     <option 
@@ -145,5 +187,66 @@ namespace wpgp
             <?php
         }
 
+        /**
+         * Register the main menu option under the Settings menu
+         * 
+         * @return void
+         */
+        public static function addMenuPage() : void
+        {
+            add_submenu_page(
+                'options-general.php',
+                'WP Google Places',
+                'WP Google Places',
+                'manage_options',
+                self::PAGE_NAME,
+                [self::class, 'renderPage']
+            );
+        }
+
+        /**
+         * Show the main options menu settings page
+         * 
+         * @return void
+         */
+        public static function renderPage() : void 
+        {
+            // check user capabilities
+            if (!current_user_can('manage_options')) {
+                return;
+            }
+            
+            // check if the user have submitted the settings
+            // WordPress will add the "settings-updated" $_GET parameter
+            // to the url
+            if (isset($_GET['settings-updated'])) {
+                // add settings saved message with the class of "updated"
+                add_settings_error(
+                    'wpgp_messages',
+                    'wpgp_message',
+                    __('Settings Saved', 'wpgp'),
+                    'updated'
+                );
+            }
+            // show error/update messages
+            settings_errors('wporg_messages');
+            ?>
+            <div class="wrap">
+                <h1><?php echo esc_html(get_admin_page_title()); ?></h1>
+                <form action="options.php" method="post">
+                <?php
+                // output security fields for the registered setting "wpgp"
+                settings_fields(self::PAGE_NAME);
+                // output setting sections and their fields
+                // (sections are registered for "wpgp", 
+                // each field is registered to a specific section)
+                do_settings_sections(self::PAGE_NAME);
+                // output save settings button
+                submit_button('Save Settings');
+                ?>
+                </form>
+            </div>
+            <?php
+        }
     }
 }
